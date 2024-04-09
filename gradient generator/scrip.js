bd022dad2de07a08d1df88eb886815eab8f20c43
Joy Mojumder Dip <166296999+Joy-Mojumder@users.noreply.github.com>
2024-04-09 23:03:26 +0600

    adding javascript project

---
+++ gradient generator/scrip.js
@@ -0,0 +1,36 @@
+const btnOne = document.querySelector('.btn-1');
+const btnTwo = document.querySelector('.btn-2');
+const copyPara =document.querySelector('.copy-code');
+const randomCode = () => {
+let colorCode = "0123456789ABCDEF";
+let finalCode = "#";
+for(num = 0;num<6;num++){
+    // console.log(colorCode[Math.trunc(Math.random()*16)]);
+    finalCode = finalCode + `${colorCode[Math.trunc(Math.random()*16)]}`;
+}
+return finalCode;
+};
+
+
+
+const clickBtnOne = () =>{
+ let leftBtn = randomCode();
+   document.body.style.backgroundImage=`linear-gradient(to right top, ${leftBtn}, ${btnTwo.textContent})`;
+   copyPara.textContent = `background-image: linear-gradient(to right top, ${leftBtn}, ${btnTwo.textContent})`;
+   btnOne.textContent = `${leftBtn}`
+   btnOne.style.backgroundColor = `${leftBtn}`;
+   return leftBtn;
+}
+const clickBtnTwo = () =>{
+    let rightBtn = randomCode();
+    document.body.style.backgroundImage=`linear-gradient(to right top, ${btnOne.textContent}, ${rightBtn})`;
+    copyPara.textContent = `background-image: linear-gradient(to right top, ${btnOne.textContent}, ${rightBtn})`;
+    btnTwo.textContent = `${rightBtn}`
+    btnTwo.style.backgroundColor = `${rightBtn}`;
+}
+copyPara.addEventListener("click",() =>{
+    navigator.clipboard.writeText(copyPara.textContent);
+})
+
+btnOne.addEventListener("click",clickBtnOne);
+btnTwo.addEventListener("click",clickBtnTwo);
