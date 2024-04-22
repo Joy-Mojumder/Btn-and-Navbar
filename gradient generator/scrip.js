9b26ee73119618fb0b06cf29178649930a35c214
Joy Mojumder Dip <166296999+Joy-Mojumder@users.noreply.github.com>
2024-04-22 18:07:59 +0600

    adding reusable functionality

--- gradient generator/scrip.js
+++ gradient generator/scrip.js
@@ -1,30 +1,29 @@
 const btnOne = document.querySelector('.btn-1');
 const btnTwo = document.querySelector('.btn-2');
 const copyPara =document.querySelector('.copy-code');
+
 const randomCode = () => {
 let colorCode = "0123456789ABCDEF";
 let finalCode = "#";
 for(num = 0;num<6;num++){
-    // console.log(colorCode[Math.trunc(Math.random()*16)]);
     finalCode = finalCode + `${colorCode[Math.trunc(Math.random()*16)]}`;
 }
 return finalCode;
 }
+const BodyBackgroundImgGradient = (leftElem,rightElem,btn,changeElem) => {
+    document.body.style.backgroundImage=`linear-gradient(to right top, ${leftElem}, ${rightElem})`;
+    copyPara.textContent = `background-image: linear-gradient(to right top, ${leftElem}, ${rightElem})`;
+    btn.textContent = `${changeElem}`;
+    btn.style.backgroundColor = `${changeElem}`;
+}
 
 const clickBtnOne = () =>{
  let leftBtn = randomCode();
-   document.body.style.backgroundImage=`linear-gradient(to right top, ${leftBtn}, ${btnTwo.textContent})`;
-   copyPara.textContent = `background-image: linear-gradient(to right top, ${leftBtn}, ${btnTwo.textContent})`;
-   btnOne.textContent = `${leftBtn}`;
-   btnOne.style.backgroundColor = `${leftBtn}`;
-   return leftBtn;
+   BodyBackgroundImgGradient(leftBtn,btnTwo.textContent,btnOne,leftBtn);
 }
 const clickBtnTwo = () =>{
     let rightBtn = randomCode();
-    document.body.style.backgroundImage=`linear-gradient(to right top, ${btnOne.textContent}, ${rightBtn})`;
-    copyPara.textContent = `background-image: linear-gradient(to right top, ${btnOne.textContent}, ${rightBtn})`;
-    btnTwo.textContent = `${rightBtn}`;
-    btnTwo.style.backgroundColor = `${rightBtn}`;
+    BodyBackgroundImgGradient(btnOne.textContent,rightBtn,btnTwo,rightBtn);
 }
 copyPara.addEventListener("click",() =>{
     navigator.clipboard.writeText(copyPara.textContent);
