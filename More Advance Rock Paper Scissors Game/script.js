7ef2c73592b3b855b27c0fa29efe6421c35223d5
Joy Mojumder Dip <166296999+Joy-Mojumder@users.noreply.github.com>
2024-04-26 14:44:49 +0600

    adding popUps for Winning

--- More Advance Rock Paper Scissors Game/script.js
+++ More Advance Rock Paper Scissors Game/script.js
@@ -4,12 +4,26 @@ const pRsContainer = document.querySelector('.paper-rock-scissors--section');
 const paperDiv = document.querySelector('.paper-div');
 const scissorsDiv = document.querySelector('.scissors-div');
 const rockDiv = document.querySelector('.rock-div');
+const popUpYou = document.querySelector('.popUpFor-you--Div');
+const popUpCom = document.querySelector('.popUpFor-com--Div');
 
 const createElem = document.createElement('div');
 createElem.classList.add("grid-three--cols");
 let numYou = 0;
 let numCom = 0;
-
+console.log(numCom);
+const popUpWinOrLoose = (you,com) =>{
+    if(you >= 10 && com < 10){
+        popUpYou.style.display = "grid";
+        pRsContainer.style.display = "none";
+        createElem.style.display = "none";
+    }else if(you < 10 && com >= 10){
+        popUpCom.style.display = "grid";
+        pRsContainer.style.display = "none";
+        createElem.style.display = "none";
+    }
+};
+popUpWinOrLoose();
 const reusableFuncAll = (classN,innerH) =>{
     let reusableVar  = document.createElement('div');
     reusableVar.classList.add(classN);
@@ -32,6 +46,18 @@ const createRockFunc = (heading) => {
     reusableFuncAll("grid-rock--div",`<p>${heading} PICKED</p>
     <img src="./images/rock-game-vector-3959189.png" alt="rock image" width="63%" height="auto">`);
 }
+const reusableIfElseFunc = (d1,d2,d3) => {
+    if(d1===d2){
+        winStatusFunc("BOTH DRAW");
+    }else if(d1===d3){
+        winStatusFunc("YOU LOOSE");
+        scoreComBoard.textContent = `${++numCom}/10`;
+    }else{
+        winStatusFunc("YOU WIN");
+        scoreBoard.textContent = `${++numYou}/10`;
+    }
+    popUpWinOrLoose(numYou,numCom);
+} 
 const randomElemFunction = (difference) => {
     let allElem = ["paper","scissors","rock"];
     let randomElem = Math.trunc(Math.random()*3);
@@ -39,37 +65,13 @@ const randomElemFunction = (difference) => {
     console.log(randomAllElem);
     switch (randomAllElem) {
         case "paper":createPaperFunc("COMPUTER");
-        if(difference===randomAllElem){
-            winStatusFunc("BOTH DRAW");
-        }else if(difference==="rock"){
-            winStatusFunc("YOU LOOSE");
-            scoreComBoard.textContent = ++numCom;
-        }else if(difference==="scissors"){
-            winStatusFunc("YOU WIN");
-            scoreBoard.textContent = ++numYou;
-        }
+        reusableIfElseFunc(difference,randomAllElem,"rock")
             break;
         case "scissors":createScissorsFunc("COMPUTER");
-        if(difference===randomAllElem){
-            winStatusFunc("BOTH DRAW");
-        }else if(difference==="paper"){
-            winStatusFunc("YOU LOOSE");
-            scoreComBoard.textContent = ++numCom;
-        }else if(difference==="rock"){
-            winStatusFunc("YOU WIN");
-            scoreBoard.textContent = ++numYou;
-        }
+        reusableIfElseFunc(difference,randomAllElem,"paper")
             break;
         case "rock":createRockFunc("COMPUTER");
-        if(difference===randomAllElem){
-            winStatusFunc("BOTH DRAW");
-        }else if(difference==="scissors"){
-            winStatusFunc("YOU LOOSE");
-            scoreComBoard.textContent = ++numCom;
-        }else if(difference==="paper"){
-            winStatusFunc("YOU WIN");
-            scoreBoard.textContent = ++numYou;
-        }
+        reusableIfElseFunc(difference,randomAllElem,"scissors")
             break;
     }
     }
@@ -92,10 +94,19 @@ createElem.addEventListener("click",(event) => {
 if(event.target.classList.contains("btn")){
     pRsContainer.style.display = "grid";
     createElem.remove();
-  let children = createElem.childNodes;
-    children.forEach((curElem) => {
-        curElem.style.display = "none";
-    })
+    createElem.firstElementChild.nextElementSibling.remove();
+    createElem.firstElementChild.remove();
+    createElem.lastElementChild.remove();
+}
+})
+popUpYou.addEventListener("click",(event) => {
+if(event.target.classList.contains("btn")){
+   navigation.reload();
+}
+})
+popUpCom.addEventListener("click",(event) => {
+if(event.target.classList.contains("btn")){
+   navigation.reload();
 }
 })
 paperDiv.addEventListener("click",paperClickEvent);
